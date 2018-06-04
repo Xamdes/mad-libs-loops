8f1c0ad09712454646088c0fd3875b8c3dce0c57
Jean Jia and Steven Colburn <steven@>
2018-06-04 11:05:58 -0700

    optomized using a foreach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,32 @@
-function setText(what,text)
+//Sets the text using what to define what you are changing using the value given.
+function setText(what,value)
 {
-  $(what).text(text);
+  $(what).text(value);
 }
 
 function submitForm(formID)
 {
   $(formID).submit(function(event)
   {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    //var begin = ["input#","."];
+    var inputs = ["person1","person2","animal","exclamation","verb","noun"];
+    inputs.forEach(function(input) {
+      var value = $("input#"+input).val();
+      setText("."+input,value);
+    });
+    // var person1Input = $("input#person1").val();
+    // var person2Input = $("input#person2").val();
+    // var animalInput = $("input#animal").val();
+    // var exclamationInput = $("input#exclamation").val();
+    // var verbInput = $("input#verb").val();
+    // var nounInput = $("input#noun").val();
 
-    setText(".person1",person1Input);
-    setText(".person2",person2Input);
-    setText(".animal",animalInput);
-    setText(".exclamation",exclamationInput);
-    setText(".verb",verbInput);
-    setText(".noun",nounInput);
+    // setText(".person1",person1Input);
+    // setText(".person2",person2Input);
+    // setText(".animal",animalInput);
+    // setText(".exclamation",exclamationInput);
+    // setText(".verb",verbInput);
+    // setText(".noun",nounInput);
     showStory("#story");
     event.preventDefault();
   });
